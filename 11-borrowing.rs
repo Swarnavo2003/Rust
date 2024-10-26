33d8b5fbd6d17f618f7a0804d4466a31cf0efab4
Swarnabha Majumder <91934646+Swarnavo2003@users.noreply.github.com>
2024-10-26 10:13:34 +0530

    Update 11-borrowing.rs

--- 11-borrowing.rs
+++ 11-borrowing.rs
@@ -1,8 +1,10 @@
 fn main() {
-  let s1 = String::from("swarnavo");
-  do_something(&s1)
+  let mut s1 = String::from("swarnavo");
+  do_something(&mut s1);
+  println!("name is {}", s1);
 }
 
-fn do_something(s2:&String) {
+fn do_something(s2:&mut String) {
+  s2.push_str(" majumder");
   println!("{}", s2);
 }
